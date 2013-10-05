ebac03a0f950af1688feba71084f0e1fb1e517b3
Christopher Ogden <ogdenchris@gmail.com>
2013-10-05 15:36:59 -0400

    Fixed and now climbs tree until it gets a classed element.

--- selection.js
+++ selection.js
@@ -9,21 +9,19 @@ jQuery.noConflict();
 var selectionSelectors = function(){
     var selObj = window.getSelection();
     var range  = selObj.getRangeAt(0);
-    var selector = range.startContainer.parentNode;   // TODO: may not work in IE, see http://stackoverflow.com/questions/1335252/how-can-i-get-the-dom-element-which-contains-the-current-selection
-    var classesAndIDs = new Array();
-    
-    classesAndIDs.push($(selector).css('class'));
-    classesAndIDs.push($(selector).css('id'));
-    
-    //test
-    console.log(hasClassOrID(classesAndIDs));
- 
-    console.log(selObj);
-    console.log(range);
-    console.log(classesAndIDs);
+    var selector = range.startContainer.parentNode;    
+    while(!hasClass(selector)){
+        // TODO: what if there are no classes anywhere, infinite loop?
+        selector = $(selector).parent();
+    }
+   
+    return $(selector).attr('class');
 }
 
-var hasClassOrID = function(node){
-    if(node.id || node.classList) return true;
-    else return false;
+var hasClass = function(node){
+    console.log($(node));
+    if($(node).attr('class')) 
+        return true;
+    else 
+        return false;
 }
